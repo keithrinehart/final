95b94f7a4350debe4d550a3d51f7d594684656eb
Keith Rinehart <rinehartkeith100@gmail.com>
2021-01-25 15:26:37 +0100

    added some effects on landing page

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { TweenMax, Power3 } from "gsap";
 import Face from "./img/face.jpg";
 import Hamburger from "./components/Hamburger";
 import Section2Home from "./components/Section2Home";
 
 function Home() {
+  let headingh1 = useRef(null);
+  let headingp = useRef(null);
+  let headingimg = useRef(null);
+  let headingBottomLine = useRef(null);
+
+  useEffect(() => {
+    TweenMax.from(headingh1, 1.5, {
+      opacity: 0,
+      x: 100,
+      ease: Power3.easeOut,
+      delay: 1,
+    });
+    TweenMax.from(headingp, 1.5, {
+      opacity: 0,
+      y: 100,
+      ease: Power3.easeOut,
+      delay: 1.5,
+    });
+    TweenMax.from(headingimg, 1.5, {
+      opacity: 0,
+      x: 100,
+      ease: Power3.easeOut,
+      delay: 2.1,
+    });
+    TweenMax.from(headingBottomLine, 1.8, {
+      opacity: 0,
+      y: 100,
+      ease: Power3.easeOut,
+      delay: 2.5,
+    });
+  }, []);
   return (
     <header className="App-header">
       <Hamburger />
       <div className="header-container">
-        <h1 id="homeh1">
+        <h1 ref={(el) => (headingh1 = el)} id="homeh1">
           Artsy<span className="span-homeh1">Ish</span>
         </h1>
         <div className="home-display-row">
           <div className="p-line"></div>
-          <p className="home-p1">
+          <p ref={(el) => (headingp = el)} className="home-p1">
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
             et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
             Lorem ipsum dolor sit amet. Lorem ipsum invidunt ut labore et
           </p>
-          <div className="p-line2"></div>
-          <img className="face-img" src={Face}></img>
+          <div ref={(el) => (headingBottomLine = el)} className="p-line2"></div>
+          <img
+            ref={(el) => (headingimg = el)}
+            className="face-img"
+            src={Face}
+            alt="graffiti painted face"
+          ></img>
         </div>
       </div>
       <Section2Home />
