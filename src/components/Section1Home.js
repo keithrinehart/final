3f28a1ec9efb4f3537b98abf04663cf25d419c21
Keith Rinehart <rinehartkeith100@gmail.com>
2021-01-27 10:16:24 +0100

    added sizing and section one for landing page

---
+++ src/components/Section1Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/section1home.css";
+
+function Section1Home() {
+  return (
+    <div className="section-one-container">
+      <div className="section1home-bkg">
+        <div className="img-cover">
+          <h1 className="section-one-h1">A Place To Share Ideas</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Section1Home;
