224377c6fb79e70d8618f0861f2323a4a672f4a5
Keith Rinehart <rinehartkeith100@gmail.com>
2021-02-06 06:54:12 +0100

    added section2 animation and responsive to 1200px

--- src/components/Section2Home.js
+++ src/components/Section2Home.js
@@ -1,11 +1,384 @@
 import React from "react";
 import "../css/section2home.css";
 import Img1 from "../img/img1.jpg";
+import Img17 from "../img/img-17.jpg";
 import { Link } from "react-router-dom";
 
 function Section2Home() {
   return (
     <div className="section2home-container sectionone">
+      <div className="section2-hover-container">
+        <div className="section2-hover-inner-container">
+          <div className="sec2-img-2"></div>
+          {/*<img className="sec2-img-2" src={Img17} alt="" width="400px"></img>*/}
+          <div className="sec2-p-flex">
+            <p className="sec2-hover-p">WhO SAID YOU CAnT BE An</p>
+            <p className="sec2-hover-p2">ArTIST</p>
+          </div>
+          <div className="block-one-cover"></div>
+          <div className="sec2-hover-flex-row-container">
+            <div className="sec2-hover-flex-col">
+              <div className="block1-one"></div>
+            </div>
+            <div className="sec2-hover-flex-col2">
+              <div className="block1-two"></div>
+              <div className="block2-two"></div>
+            </div>
+            <div className="sec2-hover-flex-col3">
+              <div className="block1-three"></div>
+              <div className="block2-three"></div>
+              <div className="block3-three"></div>
+            </div>
+            <div className="sec2-hover-flex-col4">
+              <div className="block1-four"></div>
+              <div className="block2-four"></div>
+              <div className="block3-four"></div>
+              <div className="block4-four"></div>
+            </div>
+            <div className="sec2-hover-flex-col5">
+              <div className="block1-five"></div>
+              <div className="block2-five"></div>
+              <div className="block3-five"></div>
+              <div className="block4-five"></div>
+              <div className="block5-five"></div>
+            </div>
+            <div className="sec2-hover-flex-col6">
+              <div className="block1-six"></div>
+              <div className="block2-six"></div>
+              <div className="block3-six"></div>
+              <div className="block4-six"></div>
+              <div className="block5-six"></div>
+              <div className="block6-six"></div>
+            </div>
+            <div className="sec2-hover-flex-col7">
+              <div className="block1-seven"></div>
+              <div className="block2-seven"></div>
+              <div className="block3-seven"></div>
+              <div className="block4-seven"></div>
+              <div className="block5-seven"></div>
+              <div className="block6-seven"></div>
+              <div className="block7-seven"></div>
+            </div>
+            <div className="sec2-hover-flex-col8">
+              <div className="block1-eight"></div>
+              <div className="block2-eight"></div>
+              <div className="block3-eight"></div>
+              <div className="block4-eight"></div>
+              <div className="block5-eight"></div>
+            </div>
+            <div className="sec2-hover-flex-col9">
+              <div className="block1-nine"></div>
+              <div className="block2-nine"></div>
+              <div className="block3-nine"></div>
+              <div className="block4-nine"></div>
+              <div className="block5-nine"></div>
+            </div>
+            <div className="sec2-hover-flex-col10">
+              <div className="block1-ten"></div>
+              <div className="block2-ten"></div>
+              <div className="block3-ten"></div>
+              <div className="block4-ten"></div>
+              <div className="block5-ten"></div>
+              <div className="block6-ten"></div>
+              <div className="block7-ten"></div>
+            </div>
+            <div className="sec2-hover-flex-col11">
+              <div className="block1-eleven"></div>
+              <div className="block2-eleven"></div>
+              <div className="block3-eleven"></div>
+              <div className="block4-eleven"></div>
+              <div className="block5-eleven"></div>
+              <div className="block6-eleven"></div>
+              <div className="block7-eleven"></div>
+            </div>
+            <div className="sec2-hover-flex-col12">
+              <div className="block1-twelve"></div>
+              <div className="block2-twelve"></div>
+              <div className="block3-twelve"></div>
+              <div className="block4-twelve"></div>
+              <div className="block5-twelve"></div>
+              <div className="block6-twelve"></div>
+              <div className="block7-twelve"></div>
+              <div className="block8-twelve"></div>
+            </div>
+            <div className="sec2-hover-flex-col13">
+              <div className="block1-thirteen"></div>
+              <div className="block2-thirteen"></div>
+              <div className="block3-thirteen"></div>
+              <div className="block4-thirteen"></div>
+              <div className="block5-thirteen"></div>
+              <div className="block6-thirteen"></div>
+              <div className="block7-thirteen"></div>
+              <div className="block8-thirteen"></div>
+              <div className="block9-thirteen"></div>
+            </div>
+            <div className="sec2-hover-flex-col14">
+              <div className="block1-fourteen"></div>
+              <div className="block2-fourteen"></div>
+              <div className="block3-fourteen"></div>
+              <div className="block4-fourteen"></div>
+              <div className="block5-fourteen"></div>
+              <div className="block6-fourteen"></div>
+              <div className="block7-fourteen"></div>
+              <div className="block8-fourteen"></div>
+              <div className="block9-fourteen"></div>
+              <div className="block10-fourteen"></div>
+            </div>
+            <div className="sec2-hover-flex-col15">
+              <div className="block1-fifteen"></div>
+              <div className="block2-fifteen"></div>
+              <div className="block3-fifteen"></div>
+              <div className="block4-fifteen"></div>
+              <div className="block5-fifteen"></div>
+              <div className="block6-fifteen"></div>
+              <div className="block7-fifteen"></div>
+              <div className="block8-fifteen"></div>
+              <div className="block9-fifteen"></div>
+              <div className="block10-fifteen"></div>
+              <div className="block11-fifteen"></div>
+            </div>
+            <div className="sec2-hover-flex-col16">
+              <div className="block1-sixteen"></div>
+              <div className="block2-sixteen"></div>
+              <div className="block3-sixteen"></div>
+              <div className="block4-sixteen"></div>
+              <div className="block5-sixteen"></div>
+              <div className="block6-sixteen"></div>
+              <div className="block7-sixteen"></div>
+              <div className="block8-sixteen"></div>
+              <div className="block9-sixteen"></div>
+            </div>
+            <div className="sec2-hover-flex-col17">
+              <div className="block1-17"></div>
+              <div className="block2-17"></div>
+              <div className="block3-17"></div>
+              <div className="block4-17"></div>
+              <div className="block5-17"></div>
+              <div className="block6-17"></div>
+            </div>
+            <div className="sec2-hover-flex-col18">
+              <div className="block1-18"></div>
+              <div className="block2-18"></div>
+              <div className="block3-18"></div>
+              <div className="block4-18"></div>
+            </div>
+            <div className="sec2-hover-flex-col19">
+              <div className="block1-19"></div>
+              <div className="block2-19"></div>
+            </div>
+            <div className="sec2-hover-flex-col20">
+              <div className="block1-20"></div>
+              <div className="block2-20"></div>
+              <div className="block3-20"></div>
+              <div className="block4-20"></div>
+              <div className="block5-20"></div>
+              <div className="block6-20"></div>
+            </div>
+            <div className="sec2-hover-flex-col21">
+              <div className="block1-21"></div>
+              <div className="block2-21"></div>
+              <div className="block3-21"></div>
+              <div className="block4-21"></div>
+            </div>
+            <div className="sec2-hover-flex-col22">
+              <div className="block1-22"></div>
+              <div className="block2-22"></div>
+              <div className="block3-22"></div>
+              <div className="block4-22"></div>
+              <div className="block5-22"></div>
+              <div className="block6-22"></div>
+              <div className="block7-22"></div>
+            </div>
+            <div className="sec2-hover-flex-col23">
+              <div className="block1-23"></div>
+              <div className="block2-23"></div>
+              <div className="block3-23"></div>
+              <div className="block4-23"></div>
+              <div className="block5-23"></div>
+              <div className="block6-23"></div>
+              <div className="block7-23"></div>
+              <div className="block8-23"></div>
+              <div className="block9-23"></div>
+            </div>
+            <div className="sec2-hover-flex-col24">
+              <div className="block1-24"></div>
+              <div className="block2-24"></div>
+              <div className="block3-24"></div>
+              <div className="block4-24"></div>
+              <div className="block5-24"></div>
+              <div className="block6-24"></div>
+              <div className="block7-24"></div>
+              <div className="block8-24"></div>
+              <div className="block9-24"></div>
+              <div className="block10-24"></div>
+            </div>
+            <div className="sec2-hover-flex-col25">
+              <div className="block1-25"></div>
+              <div className="block2-25"></div>
+              <div className="block3-25"></div>
+              <div className="block4-25"></div>
+              <div className="block5-25"></div>
+              <div className="block6-25"></div>
+              <div className="block7-25"></div>
+              <div className="block8-25"></div>
+              <div className="block9-25"></div>
+              <div className="block10-25"></div>
+              <div className="block11-25"></div>
+              <div className="block12-25"></div>
+            </div>
+            <div className="sec2-hover-flex-col26">
+              <div className="block1-26"></div>
+              <div className="block2-26"></div>
+              <div className="block3-26"></div>
+              <div className="block4-26"></div>
+              <div className="block5-26"></div>
+              <div className="block6-26"></div>
+              <div className="block7-26"></div>
+              <div className="block8-26"></div>
+            </div>
+            <div className="sec2-hover-flex-col27">
+              <div className="block1-27"></div>
+              <div className="block2-27"></div>
+              <div className="block3-27"></div>
+              <div className="block4-27"></div>
+              <div className="block5-27"></div>
+              <div className="block6-27"></div>
+              <div className="block7-27"></div>
+              <div className="block8-27"></div>
+              <div className="block9-27"></div>
+              <div className="block10-27"></div>
+            </div>
+            <div className="sec2-hover-flex-col28">
+              <div className="block1-28"></div>
+              <div className="block2-28"></div>
+              <div className="block3-28"></div>
+              <div className="block4-28"></div>
+              <div className="block5-28"></div>
+              <div className="block6-28"></div>
+              <div className="block7-28"></div>
+            </div>
+            <div className="sec2-hover-flex-col29">
+              <div className="block1-29"></div>
+              <div className="block2-29"></div>
+              <div className="block3-29"></div>
+              <div className="block4-29"></div>
+              <div className="block5-29"></div>
+              <div className="block6-29"></div>
+              <div className="block7-29"></div>
+              <div className="block8-29"></div>
+              <div className="block9-29"></div>
+            </div>
+            <div className="sec2-hover-flex-col30">
+              <div className="block1-30"></div>
+              <div className="block2-30"></div>
+              <div className="block3-30"></div>
+              <div className="block4-30"></div>
+              <div className="block5-30"></div>
+              <div className="block6-30"></div>
+              <div className="block7-30"></div>
+              <div className="block8-30"></div>
+              <div className="block9-30"></div>
+              <div className="block10-30"></div>
+              <div className="block11-30"></div>
+            </div>
+            <div className="sec2-hover-flex-col31">
+              <div className="block1-31"></div>
+              <div className="block2-31"></div>
+              <div className="block3-31"></div>
+              <div className="block4-31"></div>
+              <div className="block5-31"></div>
+              <div className="block6-31"></div>
+              <div className="block7-31"></div>
+              <div className="block8-31"></div>
+              <div className="block9-31"></div>
+              <div className="block10-31"></div>
+              <div className="block11-31"></div>
+              <div className="block12-31"></div>
+              <div className="block13-31"></div>
+            </div>
+            <div className="sec2-hover-flex-col32">
+              <div className="block1-32"></div>
+              <div className="block2-32"></div>
+              <div className="block3-32"></div>
+              <div className="block4-32"></div>
+              <div className="block5-32"></div>
+              <div className="block6-32"></div>
+              <div className="block7-32"></div>
+              <div className="block8-32"></div>
+              <div className="block9-32"></div>
+              <div className="block10-32"></div>
+              <div className="block11-32"></div>
+              <div className="block12-32"></div>
+              <div className="block13-32"></div>
+              <div className="block14-32"></div>
+            </div>
+            <div className="sec2-hover-flex-col33">
+              <div className="block1-33"></div>
+              <div className="block2-33"></div>
+              <div className="block3-33"></div>
+              <div className="block4-33"></div>
+              <div className="block5-33"></div>
+              <div className="block6-33"></div>
+              <div className="block7-33"></div>
+              <div className="block8-33"></div>
+              <div className="block9-33"></div>
+            </div>
+            <div className="sec2-hover-flex-col34">
+              <div className="block1-34"></div>
+              <div className="block2-34"></div>
+              <div className="block3-34"></div>
+              <div className="block4-34"></div>
+              <div className="block5-34"></div>
+              <div className="block6-34"></div>
+              <div className="block7-34"></div>
+              <div className="block8-34"></div>
+              <div className="block9-34"></div>
+              <div className="block10-34"></div>
+              <div className="block11-34"></div>
+              <div className="block12-34"></div>
+              <div className="block13-34"></div>
+              <div className="block14-34"></div>
+              <div className="block15-34"></div>
+              <div className="block16-34"></div>
+            </div>
+            <div className="sec2-hover-flex-col35">
+              <div className="block1-35"></div>
+              <div className="block2-35"></div>
+              <div className="block3-35"></div>
+              <div className="block4-35"></div>
+              <div className="block5-35"></div>
+              <div className="block6-35"></div>
+              <div className="block7-35"></div>
+              <div className="block8-35"></div>
+              <div className="block9-35"></div>
+              <div className="block10-35"></div>
+              <div className="block11-35"></div>
+              <div className="block12-35"></div>
+              <div className="block13-35"></div>
+            </div>
+            <div className="sec2-hover-flex-col36">
+              <div className="block1-36"></div>
+              <div className="block2-36"></div>
+              <div className="block3-36"></div>
+              <div className="block4-36"></div>
+              <div className="block5-36"></div>
+              <div className="block6-36"></div>
+              <div className="block7-36"></div>
+              <div className="block8-36"></div>
+              <div className="block9-36"></div>
+              <div className="block10-36"></div>
+              <div className="block11-36"></div>
+              <div className="block12-36"></div>
+              <div className="block13-36"></div>
+              <div className="block14-36"></div>
+              <div className="block15-36"></div>
+              <div className="block16-36"></div>
+              <div className="block17-36"></div>
+              <div className="block18-36"></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex">
         <img className="img1" src={Img1} alt="Womans painted face"></img>
         <div className="float-col">
@@ -21,6 +394,7 @@ function Section2Home() {
               <div className="float-line"></div>
             </Link>
           </ul>
+
           <div className="section-two-redlineOne"></div>
         </div>
       </div>
