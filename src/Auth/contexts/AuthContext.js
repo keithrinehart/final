6bc437a3f45c79f3464b1d30553baf1da155648b
yschnell <yl.schnell@gmail.com>
2021-02-08 22:16:02 +0100

    fixing PrivateRoute

--- src/Auth/contexts/AuthContext.js
+++ src/Auth/contexts/AuthContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { auth } from "../../firebaseConfig";
 
 
-const AuthContext = React.createContext()
+export const AuthContext = React.createContext()
 
 export function useAuth() {
   return useContext(AuthContext)
