8da14db494cd406506aaa3c63cf36a4e356b76af
Keith Rinehart <rinehartkeith100@gmail.com>
2021-02-10 17:04:04 +0100

    added animation to sectionhome1

--- src/components/Section1Home.js
+++ src/components/Section1Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "../css/section1home.css";
+import Img10 from "../img/img-10.jpg";
+import Imgvtroop from "../img/vtrooper.jpg";
+import Img6 from "../img/img-6.jpg";
+import Img16 from "../img/img-16.jpg";
 
 function Section1Home() {
   return (
@@ -15,6 +19,34 @@ function Section1Home() {
           <div className="img-cover-right"></div>
         </div>
       </div>
+      <img
+        className="section1-home-img-hover"
+        src={Img10}
+        alt="painted face of a woman"
+        width="200px"
+        height="200px"
+      ></img>
+      <img
+        className="section1-home-img-hover2"
+        src={Imgvtroop}
+        alt="painted face of a woman"
+        width="200px"
+        height="200px"
+      ></img>
+      <img
+        className="section1-home-img-hover3"
+        src={Img6}
+        alt="painted face of a woman"
+        width="200px"
+        height="200px"
+      ></img>
+      <img
+        className="section1-home-img-hover4"
+        src={Img16}
+        alt="painted face of a woman"
+        width="200px"
+        height="200px"
+      ></img>
     </div>
   );
 }
